6ee681e4aa19ecadcfc8f7f01373edffc7ba8720
Nabil Basri Hamdaoui <nabbas@alu.edu.gva.es>
2025-05-08 19:55:28 +0200

    included font size

--- app/src/main/java/com/example/gitprueba/MainActivity.kt
+++ app/src/main/java/com/example/gitprueba/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.gitprueba.ui.theme.GitPruebaTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        fontSize = 24.sp,
         modifier = modifier
     )
 }
